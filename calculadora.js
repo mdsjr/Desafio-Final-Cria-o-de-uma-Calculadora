97669cdf5477cd13f2741e82db34a62094a99b0c
domingos <mds.junior87@gmail.com>
2024-09-08 23:15:48 -0300

    :wastebasket: remove: Removendo imagem de fundo/ :bug: fix: corrigindo function calculdar

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,26 @@
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('.buttons button');
-let currentInput = '';
+function insert(num) {
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const value = button.textContent;
-    if (value === '=') {
-      try {
-        currentInput = eval(currentInput); // Tente avaliar a expressão
-        display.value = currentInput;
-      } catch (error) {
-        // Exibe mensagem de erro caso a expressão seja inválida
-        display.value = "Erro";
-      }
-    } else if (value === 'C') {
-      currentInput = '';
-      display.value = '';
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back() {
+
+  var resultado = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+}
+
+function calcular() {
+
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
-      currentInput += value;
-      display.value = currentInput;
+        document.getElementById('resultado').innerHTML = "Nada para calcular";
     }
-  });
-});
+}
